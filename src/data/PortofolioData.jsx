2f81b1fc2e0a27cc37dbba356be06fc7b22458f7
kotepgans <echo.kotepganzz2006@gmail.com>
2025-06-26 12:44:04 +0700

    data

--- src/data/PortofolioData.jsx
+++ src/data/PortofolioData.jsx
@@ -1,13 +1,32 @@
 import porto_1 from "../assets/portofolio/1.jpeg";
 import porto_2 from "../assets/portofolio/2.jpeg";
 import porto_3 from "../assets/portofolio/3.jpeg";
-
+import porto_4 from "../assets/portofolio/seltrik1.png";
 
 const portofolioData = [
   {
     id: 1,
+    title: "seltrik.id",
+    link: "https://seltrik.id",
+    image: porto_4,
+    description: "website online store yang bergerak di bidang pusat penjualan sepeda listrik.",
+  },
+  {
+    id: 2,
+    title: "Website Project 2",
+    image: porto_2,
+    description: "Ini adalah detail dari Website Project 2.",
+  },
+  {
+    id: 3,
+    title: "Poster Desain 1",
+    image: porto_3,
+    description: "Ini adalah detail dari Poster Desain 1.",
+  },
+  {
+    id: 1,
     title: "Website Project 1",
-    image: porto_1, 
+    image: porto_1,
     description: "Ini adalah detail dari Website Project 1.",
   },
   {
@@ -25,7 +44,7 @@ const portofolioData = [
   {
     id: 1,
     title: "Website Project 1",
-    image: porto_1, 
+    image: porto_1,
     description: "Ini adalah detail dari Website Project 1.",
   },
   {
@@ -40,26 +59,6 @@ const portofolioData = [
     image: porto_3,
     description: "Ini adalah detail dari Poster Desain 1.",
   },
-  {
-    id: 1,
-    title: "Website Project 1",
-    image: porto_1, 
-    description: "Ini adalah detail dari Website Project 1.",
-  },
-  {
-    id: 2,
-    title: "Website Project 2",
-    image: porto_2,
-    description: "Ini adalah detail dari Website Project 2.",
-  },
-  {
-    id: 3,
-    title: "Poster Desain 1",
-    image: porto_3,
-    description: "Ini adalah detail dari Poster Desain 1.",
-  },
-  
-
 ];
 
 export default portofolioData;
